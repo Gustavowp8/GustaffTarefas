1e8f45508bb949d133ea0bc40507fee4a77d9a3d
Gustavowp8 <gustavoferreira73@live.com>
2025-05-08 17:33:57 -0300

    Tabela de Tarefas foi criado.

---
+++ GustaffTarefas/Models/TarefaModel.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GustaffTarefas.Models
+{
+    public class TarefaModel
+    {
+
+        [Key]
+        public int TarefaId { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "O nome da tarefa não pode exceder 100 caracteres.")]
+        public string TarefaNome { get; set; }
+
+        public bool Feito { get; set; }
+
+        public DateTime Vencimento { get; set; }
+
+        public DateTime DataCriacao { get; set; } = DateTime.Now;
+
+        public string Descricao { get; set; }
+
+        public enum PrioridadeEnum
+        {
+            Baixa,
+            Media,
+            Alta
+        }
+
+        public PrioridadeEnum Prioridade { get; set; }
+
+        //idetificar o usuario
+
+        [Required]
+        public string UserId { get; set; } = "df";
+
+        [ForeignKey("UserId")]
+        public virtual IdentityUser User { get; set; }
+    }
+
+}
