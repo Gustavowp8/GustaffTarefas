d200d7427f4e285ff48dedf40e5335a69b8031c8
Gustavowp8 <gustavoferreira73@live.com>
2025-05-16 16:56:57 -0300

    Novo layout

--- GustaffTarefas/wwwroot/js/site.js
+++ GustaffTarefas/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+const btnMobile = document.getElementById('btnMobile');
+
+function toggleMenu() {
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+}
+
+btnMobile.addEventListener('click', toggleMenu);
